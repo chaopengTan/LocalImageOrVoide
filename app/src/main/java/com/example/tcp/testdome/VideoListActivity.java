e1b6aba3addafc093922fb6167762442c56305c2
Tan <1029293806@qq.com>
2017-04-08 00:59:05 +0800

    Tan00:58

--- app/src/main/java/com/example/tcp/testdome/VideoListActivity.java
+++ app/src/main/java/com/example/tcp/testdome/VideoListActivity.java
@@ -13,7 +13,6 @@ import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
